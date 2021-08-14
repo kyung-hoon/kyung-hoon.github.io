042e8a6b874af0dab63be19e4a665937ba28b548
민경훈 <min@mingyeonghun-ui-MacBookAir.local>
2021-08-14 16:23:38 +0900

    Updates

---
+++ precache-manifest.147105eeac07237a172ce74b4f13c4b9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3daf5a2a0e6eac189df8320987e0c227",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06147055c6fbe529fbea",
+    "url": "/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "e980613669b0bd0c307e",
+    "url": "/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "e6594bd9ba37deeb695f",
+    "url": "/static/css/main.7b74bbd0.chunk.css"
+  },
+  {
+    "revision": "0e5c074909ac916b260f",
+    "url": "/static/js/2.6fa483eb.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.6fa483eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06147055c6fbe529fbea",
+    "url": "/static/js/3.3f01fc1c.chunk.js"
+  },
+  {
+    "revision": "e980613669b0bd0c307e",
+    "url": "/static/js/4.d57fbf72.chunk.js"
+  },
+  {
+    "revision": "e6594bd9ba37deeb695f",
+    "url": "/static/js/main.23dd1888.chunk.js"
+  },
+  {
+    "revision": "2e188e151aafde75f6ca",
+    "url": "/static/js/runtime-main.9a642b49.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "25e2a3c0b7a0fdf7f229b79d4908cdc8",
+    "url": "/static/media/CKA.25e2a3c0.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d7b42c59ce61bda71f32e157bd977734",
+    "url": "/static/media/UI_Mark.d7b42c59.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "d469106236bfa53845bc5d55b7eece26",
+    "url": "/static/media/hooni-ico.d4691062.png"
+  },
+  {
+    "revision": "c01429efb099343e377a3a75454ea14b",
+    "url": "/static/media/hooni.c01429ef.png"
+  },
+  {
+    "revision": "558a970f15c96efd5fa8482bf361293f",
+    "url": "/static/media/kakao-ent.558a970f.png"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "58a29f864f1b0fc89612b902e2ef61a6",
+    "url": "/static/media/tmaxsoft.58a29f86.jpeg"
+  }
+]);
